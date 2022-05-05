a7a82e2981ba8b508f27ed0045ca3f035fc0cf3d
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2022-05-05 10:55:29 -0400

    Move to v6 - it works.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => (
-  <div>
+const unusedVar = "Hello!";
+
+const App = () => {
+  const [ count ] = useState(1);
+  // This should trigger an eslint error.
+  useEffect(() => console.log(`here is count: ${count}`))
+  return <div>
     <h1>Sample React App</h1>
     <div>
       This should be checked by eslint through @parcel/validator-eslint
     </div>
   </div>
-);
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
